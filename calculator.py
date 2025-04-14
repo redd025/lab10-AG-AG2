32329765c88220ddb431ad01777cbf2c5b68fc55
redd025 <alexandergordillo025@gmail.com>
2025-04-14 13:09:57 -0400

    merged changes

--- calculator.py
+++ calculator.py
@@ -7,14 +7,6 @@ One function per operation, in order.
 # First example
 import math
 
-def square_roots(a):
-    if a<0:
-        raise ValueError("Input must be positive")
-    return math.sqrt(a)
-
-def hypotenuse(a, b):
-    return math.hypot(a,b)
-
 def add(a, b):
     return a+b
 
@@ -24,6 +16,11 @@ def subtract(a, b):
 def multiply(a, b):
     return a * b
 
+def divide(a, b):
+    if a == 0:
+        raise ZeroDivisionError("Cannot divide by zero")
+    return b/a
+
 def logarithm(a, b):
     if a <= 0 or b <= 0:
         raise ValueError("Input must be greater than 0")
@@ -32,6 +29,10 @@ def logarithm(a, b):
 def exponent(a, b):
     return a**b
 
+def square_roots(a):
+    if a<0:
+        raise ValueError("Input must be positive")
+    return math.sqrt(a)
 
-
-
+def hypotenuse(a, b):
+    return math.hypot(a,b)
